2fa67db9690bb8e1fa10005d77b1b855fefacb0d
Kshitij <chaudharykshitij49@gmail.com>
2026-03-16 01:09:03 -0400

    Create Authentication System Day2

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
@@ -9,6 +10,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("FinTrack API Running...");
